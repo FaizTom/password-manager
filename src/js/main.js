6441911d4d413f3860a36eb046a77fd9f84f2c6a
fuad24k <fuad.hassan50@gmail.com>
2022-01-25 09:54:21 +0600

    Blockchain done
    Fuad

--- src/js/main.js
+++ src/js/main.js
@@ -623,7 +623,7 @@ function clicktoshow(id){
     backend.accounts[id].getPassword()
         .then(function(pwd){
             $("#"+id).empty()
-                .append($('<span class="pwdshowbox passwordText"></span>'))
+                .append($('<span class="pwdshowbox passwordText"></span><span class="emni"></span>'))
                 .after($('<a title="Hide" class="cellOptionButton hidePassword"></a>')
                     .on('click',{"index":id},function(event){clicktohide(event.data.index);})
                     .append($('<span class="glyphicon glyphicon-eye-close"></span>')))
@@ -641,10 +641,15 @@ function clicktoshow(id){
                                 });
                         }));
             $("#"+id+" > .pwdshowbox").text(pwd);
+
         })
         .catch(function(){
             $("#"+id).text("Oops, some error occurs!");
         });
+
+
+
+
 }
 function clicktohide(id){
     backend.resetTimeout();
@@ -738,3 +743,7 @@ function showdetail(index){
     callPlugins("showDetails",{"account":account, "out":s});
     $("#showdetails").modal("show");
 }
+
+
+
+
